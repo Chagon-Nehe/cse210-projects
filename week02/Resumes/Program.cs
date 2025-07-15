054bd486cd0b8b05892c1e9fcd3920bf79989c56
Chagon-Nehe <cgondwe@byupathway.edu>
2025-07-15 05:05:21 +0200

    Resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._company = "Tech Solutions Inc.";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        
+
+        Job job2 = new Job();
+        job2._company = "Innovatech Ltd.";
+        job2._jobTitle = "Systems Analyst";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+       
+
+        Resume resume = new Resume();
+        resume._name = "Chawanangwa Gondwe";
+        resume._jobs = new List<Job> { job1, job2 };
+        resume.DisplayResumeDetails();
+    
     }
 }
