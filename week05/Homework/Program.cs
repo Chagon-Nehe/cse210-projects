86daa3816346938f9f905c1348ee65f5c33f0256
Chagon-Nehe <cgondwe@byupathway.edu>
2025-08-11 21:51:07 +0200

    week 5 Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Alice Smith", "Math Homework");
+        MathAssingment mathAssignment1 = new MathAssingment("Alice Smith", "Algebra", "Section 5.2", "Problems 1-10");
+        WritingAssignment writingAssignment1 = new WritingAssignment("Alice Smith", "History", "The American Revolution");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
+        Console.WriteLine(writingAssignment1.GetSummary());
     }
 }
